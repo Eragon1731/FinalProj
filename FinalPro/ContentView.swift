3f274e222ea12d5208ead3c5af419eb9d2cfe368
Christy Ye <eragon1731@gmail.com>
2022-04-18 12:24:29 -0700

    init

---
+++ FinalPro/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  FinalPro
+//
+//  Created by Christy Ye on 4/12/22.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+		NavigationView{
+		VStack{
+			Text("Welcome").font(.title)
+			Spacer().frame(height: 40)
+			Text("Instructions")
+			Text("Add description and steps here")
+			
+			NavigationLink(destination: {ARViewContainer().edgesIgnoringSafeArea(.all)}, label: { Text("Go to ARView")})
+		}
+		}
+    }
+}
+
+
+
+
+
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+		let boxAnchor = try! BalloonSizeTest250.loadScene()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
